31de534f4873d4370ae930e5d3093e5aa808e8ab
Josh Smith <jmsmith1018@gmail.com>
2023-02-21 08:52:29 -0700

    feat: introduce controller action handling

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,12 +6,22 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debut.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [
         200,
         { "Content-Type" => "text/html" },
-        ["Hello from Ruby on Rulers!"]
+        [text]
       ]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
