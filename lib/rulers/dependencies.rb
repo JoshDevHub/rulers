30c26c5c9332f768d5896a37d82f06459c19cc2a
Josh Smith <jmsmith1018@gmail.com>
2023-03-10 21:25:45 -0700

    alter solution for `const_missing` patch

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,13 @@
 class Object
   def self.const_missing(constant_name)
+    @active_constant_lookups ||= {}
+    return nil if @active_constant_lookups[constant_name]
+
+    @active_constant_lookups[constant_name] = true
     require Rulers.to_underscore(constant_name.to_s)
-    Object.const_get(constant_name)
+    klass = Object.const_get(constant_name)
+    @active_constant_lookups[constant_name] = false
+
+    klass
   end
 end
