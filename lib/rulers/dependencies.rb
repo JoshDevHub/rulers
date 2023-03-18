81211da0cf7c8fcba9d10e586843fad39a0749d9
Josh Smith <jmsmith1018@gmail.com>
2023-03-18 11:21:03 -0600

    docs: add topic discussion for book club meeting re: zeitwerk

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,4 +1,5 @@
 class Object
+  # metaprogrammed Autoloading solution
   def self.const_missing(const_name)
     @active_constant_lookups ||= {}
     return nil if @active_constant_lookups[const_name]
@@ -10,4 +11,13 @@ class Object
 
     klass
   end
+
+  # current Rails uses Zeitwerk as autload solution https://github.com/fxn/zeitwerk
+  # Zeitwerk doesn't use `const_missing` tricks but rather:
+  # 1. Scans project tree to get filenames
+  # 2. Camelizes these names to get const names
+  # 3. Uses Ruby's own `Kernel#autoload`
+  # - user.rb => autoload(:User, "user")
+  # - RailsConf 2022 Keynote: https://www.youtube.com/watch?v=DzyGdOd_6-Y
+  # - Kernel#autoload: https://rubyapi.org/3.1/o/kernel#method-i-autoload
 end
