d320729aca78abf4f0593091382094da00a4b7c7
Josh Smith <jmsmith1018@gmail.com>
2023-03-28 11:15:14 -0600

    feat: use controller name in view file path

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -1,7 +1,7 @@
 module Rulers
   ACRONYM_UNDERSCORE_REGEX = /([A-Z]+)([A-Z][a-z])/
 
-  def self.to_understore(string)
+  def self.to_underscore(string)
     string
       .tr("::", "/")
       .gsub(ACRONYM_UNDERSCORE_REGEX, '\1_\2') # HTMLParser => html_parser
