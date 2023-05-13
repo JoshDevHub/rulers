92c15079923adf1d5deb841085a2b77863807f9d
Josh Smith <jmsmith1018@gmail.com>
2023-05-13 11:28:53 -0600

    Add saving and creating new FileModels

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -1,4 +1,4 @@
-require "multi_json"
+require "json"
 
 module Rulers
   module Model
@@ -10,7 +10,7 @@ module Rulers
         @id = File.basename(basename, ".json").to_i
 
         obj = File.read(filename)
-        @hash = MultiJson.load(obj)
+        @hash = JSON.parse(obj)
       end
 
       def [](name)
@@ -19,6 +19,33 @@ module Rulers
 
       def []=(name, value)
         @hash[name.to_s] = value
+      end
+
+      def save
+        json_data = JSON.dump(
+          {
+            submitter: attrs[:submitter],
+            quote: attrs[:quote],
+            attribution: attrs[:attribution]
+          }
+        )
+
+        File.write(@filename, json_data)
+      end
+
+      def self.create(**attrs)
+        id = Dir["db/quotes/*.json"].map { File.split(_1)[-1].to_i }.max + 1
+
+        json_data = JSON.dump(
+          {
+            submitter: attrs[:submitter] || "",
+            quote: attrs[:quote] || "",
+            attribution: attrs[:attribution] || ""
+          }
+        )
+        File.write("db/quotes/#{id}.json", json_data)
+
+        FileModel.new "db/quotes/#{id}.json"
       end
 
       def self.all
